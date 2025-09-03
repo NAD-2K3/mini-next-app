b116a7b3c945f48824ae045bded72d65e3274db9
NAD-2K3 <anhduc19112003@gmail.com>
2025-09-03 09:51:32 +0700

    Update eslint config

--- apps/next-app/next.config.js
+++ apps/next-app/next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 const plugins = [
